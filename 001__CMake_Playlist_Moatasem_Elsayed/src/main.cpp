6be7f342f1687544409eb0c773b07cbe86bd880c
Michael Ibrahim <michael.ibrahim042@gmail.com>
2023-09-13 09:04:59 +0300

    Add session 4

--- 001__CMake_Playlist_Moatasem_Elsayed/src/main.cpp
+++ 001__CMake_Playlist_Moatasem_Elsayed/src/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include "calc.hpp"
+#include "wifi.hpp"
 
 int main(void)
 {
-    std::cout << sum(1,2) << std::endl;
+    std::cout << Sum(1,2) << std::endl;
+    InitWifi();
     return(0);
 }/* main */
