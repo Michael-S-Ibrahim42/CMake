ecc8164cf394f5b12203b38e022131fc5c4ef411
Michael Ibrahim <michael.ibrahim042@gmail.com>
2023-09-17 22:35:17 +0300

    Add session 6

--- 001__CMake_Playlist_Moatasem_Elsayed/src/main.cpp
+++ 001__CMake_Playlist_Moatasem_Elsayed/src/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include "calc.hpp"
 #include "wifi.hpp"
+#include "defaultconfig.h"
 
 int main(void)
 {
-    std::cout << Sum(1,2) << std::endl;
+    std::cout << "Software Version is " << HELLOAPP_VERSION_MAJOR << '.' << HELLOAPP_VERSION_MINOR << std::endl;
     InitWifi();
+    std::cout << "Product Type is " << ProductType << std::endl;
+    std::cout << "Product Year is " << PRODUCT_YEAR << std::endl;
     return(0);
 }/* main */
