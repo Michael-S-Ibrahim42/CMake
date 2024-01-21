2444f3df483fbc8d77439787a7b24d73b14ab43e
Michael Ibrahim <michael.ibrahim042@gmail.com>
2024-01-21 21:38:10 +0200

    Add session 8

--- 001__CMake_Playlist_Moatasem_Elsayed/src/main.cpp
+++ 001__CMake_Playlist_Moatasem_Elsayed/src/main.cpp
@@ -5,12 +5,12 @@
 
 int main(void)
 {
-#ifdef DEBUGINFO
+#ifdef DEBUG
     std::cout << DEBUGINFO << std::endl;
 #endif
     std::cout << "Software Version is " << HELLOAPP_VERSION_MAJOR << '.' << HELLOAPP_VERSION_MINOR << std::endl;
     InitWifi();
     std::cout << "Product Type is " << ProductType << std::endl;
     std::cout << "Product Year is " << PRODUCT_YEAR << std::endl;
-    return(0);
+    return(0); 
 }/* main */
